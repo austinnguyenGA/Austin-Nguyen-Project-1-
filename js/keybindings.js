40e4f6b8283c406f4af658f5b6edc3e808216aa7
Austin Nguyen <austinnguyen@MacBook-Pro.local>
2021-12-15 16:42:15 -0600

    created the animation for the player

--- js/keybindings.js
+++ js/keybindings.js
@@ -1,41 +1,150 @@
-const canvas = document.getElementById('Background')
+const playerSprite1 = new Image()
+playerSprite1.src = "image/tanjiro-2.png"
+const background = new Image()
+background.src = "image/dungeonbackground.png"
+const enemey1 = new Image()
+enemy1.src = "image/zenitsu-2.png"
+
+
+
+const canvas = document.getElementById('Background1')
 const  ctx = canvas.getContext('2d')
 canvas.width = 800
 canvas.height = 500
 
-const keys = [] 
-
 const player = {
-    x: 0,
-    y: 0,
-    width: 50,
-    height: 103,
+    x: 200,
+    y: 300,
+    width: 60,
+    height: 60,
     frameX: 0,
-    frameY: 0,
-    speed: 9,
+    frameY: .5,
+    speed: 12,
     moving: false,
 }
 
-const playerSprite1 = new Image()
-playerSprite1.src = "tanjiro.png"
-// const background = new Image()
-// background.src = "background1png"
 
-function drawSprite(img, sx, sy, sw, sh, dx, dy, dw, dh){
-    ctx.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh)
+function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH){
+    ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH)
+}
+
+function handlePlayerFrame(){
+    if (player.frameX < 3) {
+        player.frameX++
+    } else {
+        player.frameX = 0
+        // player.frameY = .5
+    }
+}
+
+function handlePlayerAttackFrame1(){
+    let counter = 0;
+    const playerInterval = setInterval(()=> {
+        player.frameX++;
+        counter++;
+        if (counter === 15) {
+            clearInterval(playerInterval)
+        } 
+    }, 100);
+}
+
+function handlePlayerAttackFrame2(){
+    let counter = 0;
+    const playerInterval = setInterval(()=> {
+        player.frameX++;
+        counter++;
+        if (counter === 9) {
+            clearInterval(playerInterval)
+        } 
+    }, 200);
+}
+
+function handlePlayerAttackFrame3(){
+    let counter = 0;
+    const playerInterval = setInterval(()=> {
+        player.frameX++;
+        counter++;
+        if (counter === 7) {
+            clearInterval(playerInterval)
+        } 
+    }, 250);
+}
+
+function handlePlayerAttackFrame4(){
+    let counter = 0;
+    const playerInterval = setInterval(()=> {
+        player.frameX++;
+        counter++;
+        if (counter === 12) {
+            clearInterval(playerInterval)
+        } 
+    }, 200);
+}
+
+function movePlayer(e){
+    if (e.key === "w" && player.y > 100){
+        player.y -= player.speed
+        player.frameY = 2.89
+        handlePlayerFrame()
+    } 
+    if (e.key === "a" && player.x > 0){
+        player.x -= player.speed
+        player.frameY = 2.89
+        handlePlayerFrame()
+    }
+    if (e.key === "s" && player.y < canvas.height - player.height){
+        player.y += player.speed
+        player.frameY = 2.89
+        handlePlayerFrame()
+    }
+    if (e.key === "d" && player.x < canvas.width - player.width){
+        player.x += player.speed
+        player.frameY = 2.89
+        handlePlayerFrame()
+    }
+}    
+   
+function attack(e) {
+    if (e.key === "q") {
+        player.frameY = 6.5
+        handlePlayerAttackFrame1()
+    }
+    if (e.key === "e") {
+        player.frameY = 4.5
+        handlePlayerAttackFrame2()
+    }
+    if (e.key === "r") {
+        player.frameY = 8.4
+        player.frameX = 4
+        handlePlayerAttackFrame3()
+    }
+    if (e.key === "t") {
+        player.frameY = 10.15
+        player.frameX = 0
+        handlePlayerAttackFrame4()
+    }
 }
 
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
-    drawSprite(playerSprite, 200, 200, player.width, player.height, 0 ,0, player.width, player .height)
+    drawSprite(playerSprite1, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x , player.y, player.width, player.height)
     requestAnimationFrame(animate)
 }
 animate()
 
 window.addEventListener("keydown", function(e){
-    keys[e.keys] = true
+    console.log(e.key);
+    movePlayer(e)
+    attack(e)
+    player.moving = true
 })
+
+
 window.addEventListener("keyup", function(e){
-    delete keys[e.keys]
+    if (e.key === "a" || e.key === "s" || e.key === "d" || e.key === "w"){
+        player.moving = false 
+        player.frameY = .5
+        player.frameX = 0
+    }
 })
